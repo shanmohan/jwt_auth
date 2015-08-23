8e8c8a0b0caa7cf272346b175360b3816e719f77
shanmohan <shanmohan@gmail.com>
2015-08-23 22:17:15 +0400

    server change

---
+++ app/scripts/controllers/logout.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jwtAuthApp.controller:LogoutCtrl
+ * @description
+ * # LogoutCtrl
+ * Controller of the jwtAuthApp
+ */
+angular.module('jwtAuthApp')
+  .controller('LogoutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- server/api.js
+++ server/api.js
@@ -14,7 +14,7 @@ var jwt = require("./services/jwt.js");
 var app = express();
 app.use(bodyParser.json());
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -22,38 +22,46 @@ app.use(function(req, res, next){
 });
 
 
-
 mongoose.connect('mongodb://localhost:27017/jwtdb');
 
 //  Set the URL routing for the app
-app.post('/register', function(req, res){
+
+app.post('/jobs', function (req, res) {
+    if (!req.headers.authorization) {
+        return res.status(401).send({message: 'You are not authorized'});
+    }
+    var jobs = ["Angular Jobs", ".Net Jobs", "Java Jobs"];
+    res.json(jobs);
+});
+
+app.post('/register', function (req, res) {
 
     console.log(req.body);
 
     var payload = {
-        iss : req.hostname,
-        sub : req.body._id
+        iss: req.hostname,
+        sub: req.body._id
     };
 
-    var token = jwt.encode(payload,'MOHAN');
+    var token = jwt.encode(payload, 'MOHAN');
 
 
-    var newUser= new User.model({
-        email:req.body.email,
+    var newUser = new User.model({
+        email: req.body.email,
         password: req.body.password
     });
 
-    newUser.save(function(err){
+    newUser.save(function (err) {
         res.status(200).send({
-            user : newUser.toJSON(),
-            token : token
+            user: newUser.toJSON(),
+            token: token
         })
     });
 
 });
 
 
-  //Set the port for the app and start the server
-var server = app.listen(3000, function(){
+//Set the port for the app and start the server
+var server = app.listen(3000, function () {
     console.log('app is listening on 3000');
 });
